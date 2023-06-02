3a1e7985c2c17ba1cdab5ec0169b08216539d935
emily <emily@bacheconsulting.com>
2023-06-02 10:07:27 +0200

    make start position easier

--- c_catch2/src/sample.h
+++ c_catch2/src/sample.h
@@ -5,8 +5,6 @@
 
 bool isLeapYear(int year);
 
-int MAX_FIZZBUZZ_SIZE = 9;
-// max size of the char* result in this function is MAX_FIZZBUZZ_SIZE
 void fizzBuzz(char* result, int n);
 
 int closestToZero(int input[], int inputLength);
