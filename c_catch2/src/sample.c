89a187db40eabfc8fd8bb82abd079975894ac627
emily <emily@bacheconsulting.com>
2023-06-02 10:19:53 +0200

    sample solution

--- c_catch2/src/sample.c
+++ c_catch2/src/sample.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <limits.h>
+#include <stdlib.h>
 
 #include "sample.h"
 
 bool isLeapYear(int year)
 {
+    if (year % 400 == 0)
+        return true;
+    if (year % 100 == 0)
+        return false;
     return year % 4 == 0;
 }
 
 void fizzBuzz(char* result, int n)
 {
-    if (n % 3 == 0)
+    if (n % 15 == 0)
+        sprintf(result, "FizzBuzz");
+    else if (n % 3 == 0)
         sprintf(result, "Fizz");
+    else if (n % 5 == 0)
+        sprintf(result, "Buzz");
     else
         sprintf(result, "%d", n);
 }
@@ -21,7 +30,7 @@ int closestToZero(int input[], int inputLength)
     int min = INT_MAX;
     for (int i = 1; i < inputLength; i++)
     {
-        if (input[i] < min)
+        if (abs(input[i]) < abs(min))
         {
             min = input[i];
         }
--- c_catch2/test-catch2/sample_catch.cpp
+++ c_catch2/test-catch2/sample_catch.cpp
@@ -12,6 +12,57 @@ TEST_CASE ("Leap Years") {
     SECTION("normal year") {
         REQUIRE(isLeapYear(2003) == false);
     }
+    SECTION("normal leap year") {
+        REQUIRE(isLeapYear(2004) == true);
+    }
+    SECTION("atypical normal year") {
+        REQUIRE(isLeapYear(1900) == false);
+    }
+    SECTION("atypical leap year") {
+        REQUIRE(isLeapYear(2000) == true);
+    }
 }
 
+TEST_CASE("FizzBuzz") {
+    char result[9];
+    SECTION("normal number") {
+        fizzBuzz(result, 1);
+        REQUIRE(string(result) == "1");
+    }
+    SECTION("fizz number") {
+        fizzBuzz(result, 3);
+        REQUIRE(string(result) == "Fizz");
+    }
+    SECTION("buzz number") {
+        fizzBuzz(result, 5);
+        REQUIRE(string(result) == "Buzz");
+    }
+    SECTION("fizzbuzz number") {
+        fizzBuzz(result, 15);
+        REQUIRE(string(result) == "FizzBuzz");
+    }
+}
+
+TEST_CASE("closest to zero") {
+    SECTION("positive numbers") {
+        int input[] = {11,2,1, 9};
+        int result = closestToZero(input, 4);
+        REQUIRE(result == 1);
+    }
+    SECTION("negative numbers") {
+        int input[] = {-11,-2,-1, -9};
+        int result = closestToZero(input, 4);
+        REQUIRE(result == -1);
+    }
+    SECTION("negative and positive numbers") {
+        int input[] = {11,-2,1, 9};
+        int result = closestToZero(input, 4);
+        REQUIRE(result == 1);
+    }
+    SECTION("empty list") {
+        int input[] = {};
+        int result = closestToZero(input, 0);
+        REQUIRE(result == INT_MAX);
+    }
+}
 
